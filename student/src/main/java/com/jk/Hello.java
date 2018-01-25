2178189ded22265c6f0a2149670af0fb71f6663e
15130691812 <414907954@qq.com>
2018-01-25 22:53:01 +0800

    克隆出项目后，修改

--- student/src/main/java/com/jk/Hello.java
+++ student/src/main/java/com/jk/Hello.java
@@ -6,5 +6,8 @@ package com.jk;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
     }
 }
